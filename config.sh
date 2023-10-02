d11d4a042dd64bec4a00d3f73426a01433688a9a
Rohit Dorage <rohitdorage3@gmail.com>
2023-10-02 08:30:21 +0530

    Create app

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set core configurations
+# git config --local core.sshCommand "ssh -i /home/ommuse/.ssh/rohit/rohitdorage"
+
+# Set user configurations
+git config --local user.email "rohitdorage3@gmail.com"
+git config --local user.name "Rohit Dorage"
+# git config --local user.signingkey AE5CCAAA4B112777
+
+# Set commit configuration
+# git config --local commit.gpgsign true
+
+echo "Git configuration has been set successfully!"
